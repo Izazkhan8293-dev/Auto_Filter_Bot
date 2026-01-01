fc62d32b63c94faba1fd4a4f895cbc09bb1f8020
Izazsarbudeen <izazkhanimran005@gmail.com>
2026-01-01 16:30:42 +0300

    Update info.py

--- info.py
+++ info.py
@@ -120,9 +120,9 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/sarbudeen786') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/cinenmahub') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/sarbudeen786') # Owner link for the bot
-UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/sarbudeen786') # Update channel link for the bot
+UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'hhttps://t.me/cinenmahub') # Update channel link for the bot
 
 # ============================
 # User Configuration
