1a8ced0e8b38e4e88881cd80eba03faa629af22e
Izazsarbudeen <izazkhanimran005@gmail.com>
2026-01-04 13:46:36 +0300

    Update info.py

--- info.py
+++ info.py
@@ -114,8 +114,8 @@ SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "arolinks.com") # Shorten
 SHORTENER_API3 = environ.get("SHORTENER_API3", "06c0783bacc243d34bdbd252020860cf2cf508f0")  
 SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "arolinks.com") # Shortener website for third website
 
-TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "200")) # Time gap for two-step verification in seconds (default: 20 minutes)
-THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "200"))    
+TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "100")) # Time gap for two-step verification in seconds (default: 20 minutes)
+THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "100"))    
 
 # ============================
 # Channel & Group Links Configuration
